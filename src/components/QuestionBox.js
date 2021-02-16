e29d0ed3520ddee735cfcdeec8def05989146de0
subhendu-poddar <spoddar4444@gmail.com>
2021-02-16 12:05:00 +0530

    app ready

---
+++ src/components/QuestionBox.js
@@ -0,0 +1,23 @@
+import react, {useState} from "react";
+
+const QuestionBox = ({ question, options , selected}) => {
+    const [answers, setAnswer] = useState(options);
+    return (
+        <div className="qustionBox">
+            <div className="question">{question}</div>
+            {answers.map((text, index) => (
+            <button
+                key={index}
+                className="answerBtn"
+                onClick={() => {
+                    setAnswer([text]);
+                    selected(text);
+                }}>
+            {text}
+            </button>
+            ))}
+        </div>
+    )
+}
+
+export default QuestionBox;
